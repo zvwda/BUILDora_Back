0a051508004966f9224b9bc2de2283202ce0b75b
zvwda <zwalid327@gmail.com>
2025-08-30 15:30:23 +0300

    cors update

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const SendEmail = require("./utils/SendEmail.js"); 
- 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const SendEmail = require("./utils/SendEmail.js");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 import cors from "cors";
-
-app.use(cors({
-  origin: ["https://buildora-sepia.vercel.app/",
-  "http://localhost:5173/",],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["https://buildora-sepia.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-app.use("/Email", SendEmail)
+app.use("/Email", SendEmail);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
