94d016683e41ee87b9d3753ce14cdc1c66edbbcc
zvwda <zwalid327@gmail.com>
2025-08-30 14:50:48 +0300

    first

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const SendEmail = require("./utils/SendEmail.js"); 
+ 
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use("/Email", SendEmail)
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ utils/SendEmail.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const router = express.Router();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "addminn5889@gmail.com",
+    pass: "kptf uifk dsbx orqi",
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+router.post("/sendEmail", async (req, res) => {
+  const { to, emailData} = req.body;
+  const subject = emailData.subject; 
+  const text = emailData.text;
+  if (!Array.isArray(to) || !subject || !text) {
+    return res.status(400).json({ error: "Missing fields or 'to' is not an array" });
+  }
+
+  try {
+    const results = await Promise.all(
+      to.map((email) => {
+        const mailOptions = {
+          from: process.env.EMAIL,
+          to: email,
+          subject,
+          text,
+        };
+        return transporter.sendMail(mailOptions);
+      })
+    );
+
+    console.log("All emails sent:", results);
+    res.status(200).send("All emails sent successfully");
+  } catch (error) {
+    console.error("Error sending emails:", error);
+    res.status(500).send("Failed to send one or more emails");
+  }
+});
+
+module.exports = router;
