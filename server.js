af661059e21025adeaee0fbadf2912d249d04f25
zvwda <zwalid327@gmail.com>
2025-08-30 15:26:09 +0300

    cors

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const SendEmail = require("./utils/SendEmail.js");
 require('dotenv').config();
 
 const app = express();
+import cors from "cors";
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://buildora-sepia.vercel.app/",
+  "http://localhost:5173/",],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
